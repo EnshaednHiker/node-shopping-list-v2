167f525515c44d88d551cf4359c9d3965cabf43a
EnshaednHiker <nbingham09@gmail.com>
2017-04-02 15:15:48 -0500

    POST method for recipes added

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
 
+
+
 app.post('/shopping-list', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
   const requiredFields = ['name', 'budget'];
@@ -43,14 +45,35 @@ app.post('/shopping-list', jsonParser, (req, res) => {
     }
   }
 
-  const item = ShoppingList.create(req.body.name, req.body.budget);
-  res.status(201).json(item);
-});
 
+
+app.post('/recipes', jsonParser, (req, res) => {
+  // ensure `name` and `indredients` are in request body
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
+
+
+  const item = ShoppingList.create(req.body.name, req.body.budget);
+  res.status(201).json(item);
+});
+
+const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
+
+
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
